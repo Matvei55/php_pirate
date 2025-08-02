e19b79c244380c901366bba07044592ca2e0ae6f
Matvei55 <mosy12dunaev@gmail.com>
2025-08-02 14:17:09 +0600

    добавлены гет и сет в свойства

--- pirat_ship_box.php
+++ pirat_ship_box.php
@@ -2,104 +2,103 @@
 include_once '/home/matvei/prolects_php/generator.php';
 
 class Pirate {
-    private $name = '';
+    private $get_name = '';
     
     public function __construct(string $name) {
-        $this->name = $name;
+        $this->get_name = $name;
     }
     
     public function getName(): string {
-        return $this->name;
+        return $this->get_name;
     }
     
     public function setName(string $name): void {
-        $this->name = $name;
+        $this->get_name = $name;
     }
 }
 
 class Ship {
-    private $weight;
-    private $pirate;
-    private $boxes = [];
-    private $free_weight;
+    private $get_weight;
+    private $get_pirate;
+    private $get_boxes = [];
+    private $get_free_weight;
 
     public function __construct(float $weight, Pirate $pirate) {
         if ($weight >= 60) {
             throw new Exception("Слишком большой вес должно быть меньше 60 кг");
         }
-        $this->weight = $weight;
-        $this->free_weight = $weight;
-        $this->pirate = $pirate;
+        $this->get_weight = $weight;
+        $this->get_free_weight = $weight;
+        $this->get_pirate = $pirate;
     }
 
     
-    public function addBox(Box $box): void {
-        if ($box->getWeight() > $this->free_weight) {
+    public function getAddBox(Box $box): void {
+        if ($box->getWeight() > $this->get_free_weight) {
             throw new Exception("Коробка слишком большая " . $box->getWeight());
         }
-        $this->boxes[]=$box;
-        $this->free_weight -= $box->getWeight();
+        $this->get_boxes[]=$box;
+        $this->get_free_weight -= $box->getWeight();
     }
 
     public function getWeight(): float {
-        return $this->weight;
+        return $this->get_weight;
     }
 
     public function getPirate(): Pirate {
-        return $this->pirate;
+        return $this->get_pirate;
     }
 
     public function getBoxes():array {
-        return $this->boxes;
+        return $this->get_boxes;
     }
     
     public function getFreeWeight(): float {
-        return $this->free_weight;
+        return $this->get_free_weight;
     }
 }
 
 
 class Box {
-    private $weight= null;
-    private $content = null;
-    private $count= null;
+    private $get_weight= null;
+    private $get_content = null;
+    private $get_count= null;
 
     
     public function __construct(float $weight, $content, int $count) {
         if ($weight < 4 || $weight > 10){
             throw new Exception("вес сундука должен быть от 4 до 10");
         }
-        $this->weight = $weight;
-        $this->content = $content;
-        $this->count = $count;
+        $this->get_weight = $weight;
+        $this->get_content = $content;
+        $this->get_count = $count;
     }
 
     public function getWeight(): float {
-        return $this->weight;
+        return $this->get_weight;
     } 
 
     public function getContent() {
-        return $this->content;
+        return $this->get_content;
     }
     
      public function getCount(): int {
-        return $this->count;
+        return $this->get_count;
     }
     
     public function setWeight(float $weight):void {
         if ($weight < 4 || $weight > 10) {
             throw new Exception("вес должен быть от 4 до 10");
         }
-        $this->weight= $weight;
+        $this->get_weight= $weight;
     }
 
     public function setContent($content):void {
-        $this->content=$content;
+        $this->get_content=$content;
     }
 
     public function setCount(int $count):void {
-        $this->count = $count;
+        $this->get_count = $count;
     }
 }
-//rerer
 ?>
